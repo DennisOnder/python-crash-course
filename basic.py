9b382e52792358cecd80080a3a7e489d371728c9
Dennis Onder <webdev.dennisonder@gmail.com>
2018-10-04 19:35:12 +0200

    Basics, Variables and Data Types

--- basic.py
+++ basic.py
@@ -1,1 +1,23 @@
+# Comment
+
+'''
+Comment
+'''
+
+# Single Quotes
+print('Hello World!')
+
+# Double Quotes
 print("Hello World!")
+
+# Print substrings
+print('Hello World'[0:5])
+
+# Print Number
+print(1, 2, 3)
+
+# Print on multiple lines
+print(1, 2, 3, '\n', 4, 5, 6)
+
+# Print HDD Location
+print(r'C:\\Program Files\Folder')
---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variables
+greeting = 'Hello'
+print(greeting)
+
+# Data types
+myStr = 'Hello'
+myInt = 25
+myFloat = 2.5
+myList = [1, 2, 3, 'List of three items']
+myDict = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
+
+print(myStr, 'World 2')
+
+greeting = greeting + ' World 3'
+
+print(greeting)
